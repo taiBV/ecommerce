f2c8fd4ece902f710b979fd08fccf1c48112a519
TaiBV <107521158+DOANNHOMTOI@users.noreply.github.com>
2023-05-26 21:16:43 +0700

    d

---
+++ src/models/Product.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const {Types, Schema} = require("mongoose");
+const schema = new mongoose.Schema({
+    product_name: String,
+    product_thumb: String,
+    product_description: String,
+    product_quantity: Number,
+    product_price: Number,
+    product_attributes: {type: Schema.Types.Mixed},
+}, {
+    collection: "ProductModel",
+    timestamps: true
+});
+const schemaElectronic = new mongoose.Schema({
+    brand: String,
+}, {
+    collection: "ProductElectronic",
+    timestamps: true
+});
+const schemaClothing = new mongoose.Schema({
+    brand: String,
+    size: String,
+}, {
+    collection: "ProductClothing",
+    timestamps: true
+});
+// module.exports = mongoose.model('ProductModel', schema);
+// module.exports = mongoose.model('ProductElectronic', schemaClothing);
+// module.exports = mongoose.model('ProductClothing', schemaElectronic);
+module.exports = {
+    ProductModel: mongoose.model('ProductModel', schema),
+    ProductElectronic: mongoose.model('ProductElectronic', schemaClothing),
+    ProductClothing: mongoose.model('ProductClothing', schemaElectronic),
+}
