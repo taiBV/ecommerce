52e7d2635d4318386236b2db3316d27556c7a6bd
TaiBV <107521158+DOANNHOMTOI@users.noreply.github.com>
2023-06-04 21:05:48 +0700

    update

--- src/models/Product.js
+++ src/models/Product.js
@@ -2,12 +2,17 @@ const mongoose = require('mongoose');
 const {Types, Schema} = require("mongoose");
 const schema = new mongoose.Schema({
     product_name: String,
+    product_slug: String,
+    product_rating: Number,
     product_thumb: String,
     product_description: String,
     product_quantity: Number,
     product_price: Number,
     shop_id: String,
+    product_variation: {type: Array},
     product_attributes: {type: Schema.Types.Mixed},
+    isDraft: {type: Boolean, default: false},
+    isPublish: {type: Boolean, default: true},
 }, {
     collection: "ProductModel",
     timestamps: true
